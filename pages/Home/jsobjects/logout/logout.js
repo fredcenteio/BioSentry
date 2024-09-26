9d8e94076f023ad33a865fb732d14230a2ff31ef
Fred Centeio <centfredd06@gmail.com>
2024-09-26 18:33:08 +0000

    v7

--- pages/Home/jsobjects/logout/logout.js
+++ pages/Home/jsobjects/logout/logout.js
@@ -1,7 +1,15 @@
 export default {
-	handleLogout(){
-		storeValue('user', null);
-		navigateTo('Login');
-  }
-	
+  handleLogout() {
+    storeValue('user', null);
+    navigateTo('Login');
+  },
+  startLogoutTimer() {
+    // Define o tempo em milissegundos (por exemplo, 5 minutos)
+    const logoutTime = 1 * 60 * 1000;
+
+    setTimeout(() => {
+      this.handleLogout();
+    }, logoutTime);
+  },
+
 };
